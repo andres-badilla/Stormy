5837b1de2717bf63f0690acb0575511157eba627
Andres Badilla <andres_badilla@mckinsey.com>
2015-09-17 21:13:35 -0600

    Created the view

--- app/src/main/java/com/andresbadilla/stormy/CurrentWeather.java
+++ app/src/main/java/com/andresbadilla/stormy/CurrentWeather.java
@@ -1,5 +1,9 @@
 package com.andresbadilla.stormy;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by Andres Badilla on 9/17/2015.
  */
@@ -10,6 +14,15 @@ public class CurrentWeather {
     private double mHumidity;
     private double mPrecipChance;
     private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
 
     public String getIcon() {
         return mIcon;
@@ -57,5 +70,31 @@ public class CurrentWeather {
 
     public void setSummary(String summary) {
         mSummary = summary;
+    }
+
+    public String getFormattedTime(){
+
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+        return formatter.format(new Date(getTime()*1000));
+    }
+
+    public int getIconId(){
+        //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+
+        int iconId = R.drawable.clear_day;
+
+        if(mIcon.equals("clear-night")) iconId = R.drawable.clear_night;
+        else if(mIcon.equals("rain")) iconId = R.drawable.rain;
+        else if(mIcon.equals("snow")) iconId = R.drawable.snow;
+        else if (mIcon.equals("sleet")) iconId = R.drawable.sleet;
+        else if (mIcon.equals("wind")) iconId = R.drawable.wind;
+        else if(mIcon.equals("fog")) iconId=R.drawable.fog;
+        else if(mIcon.equals("cloudy")) iconId = R.drawable.cloudy;
+        else if(mIcon.equals("partly-cloudy")) iconId = R.drawable.partly_cloudy;
+        else iconId = R.drawable.cloudy_night;
+
+
+        return iconId;
     }
 }
