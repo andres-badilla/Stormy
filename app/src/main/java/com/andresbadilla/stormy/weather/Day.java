1f8ed5f8522fdd2e0cb509ab8e3cb90a961a5c7c
Andres Badilla <andres_badilla@mckinsey.com>
2015-09-20 20:20:29 -0600

    -Completed the weekly MVC

--- app/src/main/java/com/andresbadilla/stormy/weather/Day.java
+++ app/src/main/java/com/andresbadilla/stormy/weather/Day.java
@@ -1,9 +1,16 @@
 package com.andresbadilla.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by Andres Badilla on 9/18/2015.
  */
-public class Day {
+public class Day implements Parcelable {
 
     private long mTime;
     private String mSummary;
@@ -27,8 +34,8 @@ public class Day {
         mSummary = summary;
     }
 
-    public double getTemperatureMax() {
-        return mTemperatureMax;
+    public int getTemperatureMax() {
+        return (int)Math.round(mTemperatureMax);
     }
 
     public void setTemperatureMax(double temperatureMax) {
@@ -50,4 +57,56 @@ public class Day {
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
     }
+
+    public int getIconId(){
+        return Forecast.getIconId(mIcon);
+    }
+
+    public String getDayOfTheWeek(){
+
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+        Date dateTime = new Date(mTime*1000);
+
+        return formatter.format(dateTime);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;//Not used
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(mTime);
+        parcel.writeString(mSummary);
+        parcel.writeDouble(mTemperatureMax);
+        parcel.writeString(mIcon);
+        parcel.writeString(mTimeZone);
+    }
+
+    private Day(Parcel in){
+        mTime= in.readLong();
+        mSummary=in.readString();
+        mTemperatureMax= in.readDouble();
+        mIcon=in.readString();
+        mTimeZone=in.readString();
+    }
+
+    public Day(){
+
+    }
+
+    public static final Creator<Day> CREATOR = new Creator<Day>() {
+        @Override
+        public Day createFromParcel(Parcel parcel) {
+            return new Day(parcel);
+        }
+
+        @Override
+        public Day[] newArray(int i) {
+            return new Day[i];
+        }
+    };
+
 }
--- app/src/main/java/com/andresbadilla/stormy/weather/Forecast.java
+++ app/src/main/java/com/andresbadilla/stormy/weather/Forecast.java
@@ -1,5 +1,7 @@
 package com.andresbadilla.stormy.weather;
 
+import com.andresbadilla.stormy.R;
+
 /**
  * Created by Andres Badilla on 9/18/2015.
  */
@@ -31,5 +33,24 @@ public class Forecast {
 
     public void setDailyForecast(Day[] dailyForecast) {
         mDailyForecast = dailyForecast;
+    }
+
+    public static int getIconId(String iconString){
+        //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+
+        int iconId = R.drawable.clear_day;
+
+        if(iconString.equals("clear-night")) iconId = R.drawable.clear_night;
+        else if(iconString.equals("rain")) iconId = R.drawable.rain;
+        else if(iconString.equals("snow")) iconId = R.drawable.snow;
+        else if (iconString.equals("sleet")) iconId = R.drawable.sleet;
+        else if (iconString.equals("wind")) iconId = R.drawable.wind;
+        else if(iconString.equals("fog")) iconId=R.drawable.fog;
+        else if(iconString.equals("cloudy")) iconId = R.drawable.cloudy;
+        else if(iconString.equals("partly-cloudy")) iconId = R.drawable.partly_cloudy;
+        else iconId = R.drawable.cloudy_night;
+
+
+        return iconId;
     }
 }
