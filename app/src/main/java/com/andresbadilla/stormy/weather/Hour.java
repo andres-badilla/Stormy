ad320d5a4a60d75eece4e7370692c9cc3563b5b3
Andres Badilla <andres_badilla@mckinsey.com>
2015-09-20 23:30:16 -0600

    -Completed the hourly MVC (Release candidate)

--- app/src/main/java/com/andresbadilla/stormy/weather/Hour.java
+++ app/src/main/java/com/andresbadilla/stormy/weather/Hour.java
@@ -1,9 +1,15 @@
 package com.andresbadilla.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Andres Badilla on 9/18/2015.
  */
-public class Hour {
+public class Hour implements Parcelable {
 
     private long mTime;
     private String mSummary;
@@ -27,8 +33,8 @@ public class Hour {
         mSummary = summary;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -37,6 +43,10 @@ public class Hour {
 
     public String getIcon() {
         return mIcon;
+    }
+
+    public int getIconId(){
+        return Forecast.getIconId(mIcon);
     }
 
     public void setIcon(String icon) {
@@ -50,4 +60,48 @@ public class Hour {
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
     }
+
+    public String getHour(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        Date date = new Date(mTime*1000);
+        return formatter.format(date);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0; //Not in use
+
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(mTime);
+        parcel.writeString(mSummary);
+        parcel.writeDouble(mTemperature);
+        parcel.writeString(mIcon);
+        parcel.writeString(mTimeZone);
+    }
+
+    private Hour(Parcel in){
+        mTime=in.readLong();
+        mSummary=in.readString();
+        mTemperature = in.readDouble();
+        mIcon=in.readString();
+        mTimeZone=in.readString();
+
+    }
+
+    public Hour(){}
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel parcel) {
+            return new Hour(parcel);
+        }
+
+        @Override
+        public Hour[] newArray(int i) {
+            return new Hour[i];
+        }
+    };
 }
